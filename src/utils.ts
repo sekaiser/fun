5e170dabc05a5681ba1c768e82edcd02bd2646d4
sekadu <sebastian.kaiser@digitalunit.berlin>
2018-02-07 19:03:41 +0100

    Fixed a bug in a filter fn

--- src/utils.ts
+++ src/utils.ts
@@ -8,12 +8,12 @@ export const delay = time => new Promise(resolve => setTimeout(resolve, time));
 export const until = (cond, time) =>
   cond().then(result => result || delay(time).then(() => until(cond, time)));
 
-export const has = (object, key) =>
-  object ? Object.prototype.hasOwnProperty.call(object, key) : false;
+export const has = (o, key) =>
+  o ? Object.prototype.hasOwnProperty.call(o, key) : false;
 
 export const entries = obj => {
   const res = [];
-  for (const key of obj) {
+  for (const key in obj) {
     if (has(obj, key)) {
       res.push([key, obj[key]]);
     }
